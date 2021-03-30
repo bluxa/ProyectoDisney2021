f3beb46f2552a1d729bb8d9c481847247a38f2ed
bluxa <81200014+bluxa@users.noreply.github.com>
2021-03-29 23:58:37 -0600

    Commit Pagina_Principal Diseño (Byron)

--- ProyectoDisney2021/Presentacion/Pagina_principal.cs
+++ ProyectoDisney2021/Presentacion/Pagina_principal.cs
@@ -16,5 +16,27 @@ namespace ProyectoDisney2021.Presentacion
         {
             InitializeComponent();
         }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void Pagina_principal_Load(object sender, EventArgs e)
+        {
+            label3.Parent = pictureBox1;
+            label5.Parent = pictureBox1;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
