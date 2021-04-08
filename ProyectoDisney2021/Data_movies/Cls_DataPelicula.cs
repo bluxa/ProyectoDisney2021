856c2e462911efadd73f4039ed45fcaef6736807
PC <PC@DESKTOP-QORBUC8>
2021-04-08 08:20:06 +0200

    Creacion de clases de Peliculas y un form de Administrador Byron

---
+++ ProyectoDisney2021/Data_movies/Cls_DataPelicula.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDisney2021.Data_movies
+{
+    class Cls_DataPelicula
+    {
+    }
+}
