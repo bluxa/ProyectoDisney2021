b270eba29d2338a0e7488595160f2f4cb2745ecf
PC <PC@DESKTOP-QORBUC8>
2021-04-13 18:50:39 +0200

    Creacion de dos clases

---
+++ ProyectoDisney2021/Presentacion/Contenido_Pelicula/AxiliarPelicula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoDisney2021.Presentacion.Contenido_Pelicula
+{
+    public partial class AxiliarPelicula : Form
+    {
+        public AxiliarPelicula()
+        {
+            InitializeComponent();
+        }
+    }
+}
